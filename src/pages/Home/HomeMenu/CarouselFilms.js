dca4a6faae235bb804b17bcfc6392599f74ac38f
dhthinh7 <dhthinh7@gmail.com>
2022-10-26 18:34:46 +0700

    Get data from BE,update layout for carousel film

--- src/pages/Home/HomeMenu/CarouselFilms.js
+++ src/pages/Home/HomeMenu/CarouselFilms.js
@@ -1,65 +1,49 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Slider from "react-slick";
+import { PlayCircleOutlined } from '@ant-design/icons'
 
 export default function CarouselFilms() {
-
-  function SampleNextArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        // className={`${className} ${styleSlick['slick-prev']}`}
-        className={className}
-
-        style={{ ...style, display: "block" }}
-        onClick={onClick}
-      >
-      </div>
-
-    );
-  }
-
-  function SamplePrevArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        // className={`${className} ${styleSlick['slick-prev']}`}
-        className={className}
-
-        style={{ ...style, display: "block", left: '50px' }}
-        onClick={onClick}
-      >
-      </div>
-    );
-  }
+  let { listFilms } = useSelector(state => state.FilmManageReducer)
+  console.log("listFilms", listFilms)
 
   const settings = {
-    // // variableWidth: true,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />,
     className: "center",
     centerMode: true,
     infinite: true,
     centerPadding: "60px",
-    slidesToShow: 4,
+    slidesToShow: 1,
     speed: 500,
     rows: 2,
-    slidesPerRow: 1
+    slidesPerRow: 4,
   };
 
   // Temp render film 
   const renderFilm = () => {
-    let arrayFilms = []
-    for (let index = 0; index < 12; index++) {
-      arrayFilms.push(<div key={index} className="bk-item">
-        <div className="bg-red-300 m-3 text-center">
-          <h3 className="">{index}</h3>
+    return listFilms.map((item, index) => {
+      return <div key={index}>
+        <div className="bk-item">
+          <div className="film-img" style={{ backgroundImage: `url(${item.hinhAnh})` }}>
+            <div className="bk-overload"></div>
+            <div className="trailer">
+              <PlayCircleOutlined />
+            </div>
+          </div>
+          <div className="film-content">
+            <div className="film-name">
+              <div className="name">
+                {item.tenPhim}
+              </div>
+            </div>
+            <button className="film-button">ĐẶT VÉ</button>
+          </div>
         </div>
-      </div>)
-    }
-    return arrayFilms
+
+      </div>
+    })
   }
 
-  return <div className="bk-carousel-menu container px-5 py-10 mx-auto w-4/6 bg-slate-400">
+  return <div className="bk-carousel-menu container mx-auto w-4/6">
     <Slider {...settings}>
       {/* {renderFilms()} */}
       {renderFilm()}
